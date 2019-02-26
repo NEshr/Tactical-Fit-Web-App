cb477393b81be49d47e1e525ce1da1a214c7dd0c
Neshr <neshr09@gmail.com>
2019-02-25 20:28:41 -0600

    more console log tests

--- routes/index.js
+++ routes/index.js
@@ -101,7 +101,7 @@ router.post('/MassRoutine', async (req, res) => {
 
         let startDate = req.body.date;
         console.log(moment(startDate).utc().format('L'));
-        console.log(moment.utc().format('L'));
+        console.log(momentTz.tz.guess(startDate));
         if (moment(startDate).isBefore(moment(), 'day')) {
             throw new Error('Error: Please Choose a date that is on or after today');
         }
