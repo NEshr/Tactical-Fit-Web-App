228345134a3e9ad73e43cbce0c804a8200460896
Neshr <neshr09@gmail.com>
2019-02-27 21:14:07 -0600

    using express.static instead of serve-favicon

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ const flash = require('connect-flash');
 var favicon = require('serve-favicon')
 var path = require('path')
 
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use('/', express.static('public'))
 mongodb.createEventListeners();
 mongodb.connect();
 
